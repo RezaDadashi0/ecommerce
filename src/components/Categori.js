b178373fb200ef8cc1cc13cf97f81865298309a2
@RezaDadashi <dadashireza0@gmail.com>
2023-07-25 00:47:19 +0430

    unnecessery comments removed

--- src/components/Categori.js
+++ src/components/Categori.js
@@ -9,22 +9,12 @@ export default function Categori({ categories, onClick }) {
     "دارای تخفیف",
     "گران ترین",
   ];
-
-  // const activeCategoriClassName =
-  //   "border cursor-pointer rounded-lg m-1 px-8 py-2 text-blue-500 bg-blue-100 border-blue-100";
-  // const nonActiveCategoriClassName =
-  //   "border cursor-pointer rounded-lg m-1 px-8 py-2 text-gray-500 hover:bg-blue-100 hover:text-blue-500 transition ease";
-
+  
   return (
     <ul className="flex flex-wrap justify-center items-center pt-3 text-sm font-bold">
       {categories.map((categori, index) => (
         <li
           key={index}
-          // className={
-          //   categori.active
-          //     ? activeCategoriClassName
-          //     : nonActiveCategoriClassName
-          // }
           className={`border cursor-pointer rounded-lg m-1 px-8 py-2 transition ease ${
             categori.active
               ? "text-blue-500 bg-blue-100 border-blue-100"
